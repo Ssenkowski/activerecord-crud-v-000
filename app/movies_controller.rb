14f9d141b700fcfc922f969340f83626f6d29d39
Steven Senkowski <scsenkowski@gmail.com>
2018-01-01 15:42:51 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -1,4 +1,3 @@
-require 'pry'
 # Replace the '__' in the below methods to to make the specs pass!
 # Each '__' corresponds to a single line of code you will need to write.
 # See the example below on the left and how it should look on the right.
@@ -9,17 +8,23 @@ require 'pry'
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
-  movie.save
+  __
 end
 
 def can_be_created_with_a_hash_of_attributes
-  movie = Movie.create(attributes)
+  attributes = {
+      title: "The Sting",
+      release_date: 1973,
+      director: "George Roy Hill",
+      lead: "Paul Newman",
+      in_theaters: false
+  }
+  movie = __
 end
 
 def can_be_created_in_a_block
-  movie = Movie.create do |m|
-    m.title = "#{m.title}"
-    m.release_date = "m"
+  Movie.create do |m|
+    __
   end
 end
 
